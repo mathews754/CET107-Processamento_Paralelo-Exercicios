f8d388e8c766725596f846331b433f6e993ec5f6
Matheus Andrade <mathews754@gmail.com>
2020-11-16 06:34:59 -0300

    Implementação do código de limiarização sequencial

--- Limiarizacao/limiar_seq.py
+++ Limiarizacao/limiar_seq.py
@@ -6,43 +6,44 @@ import sys
 import time
 
 def parse_args(args):
-	if (len(args) >= 1): img_name = args[0]
-	else: quit("É necessário carregar uma imagem para executar a aplicação!")
-	
-	if (len(args) >= 2): l_inicial = int(args[1])
-	else: l_inicial = 127
-	
-	if (len(args) >= 3): diff_limite = int(args[2])
-	else: diff_limite = 5
-	
-	create_hist = False
-	is_diff_abs = True
-	is_verbose = False
-	if (len(args) >= 4):
-		for arg in args[3:]:
-			if (arg == "-h"): 
-				create_hist = True
-			if (arg == '-v'):
-				is_verbose = True
-			if (arg == "-dr"):
-				is_diff_abs = False
-			if (arg == "-da"):
-				is_diff_abs = True
-	return img_name, l_inicial, diff_limite, is_diff_abs, create_hist, is_verbose 
+    if (len(args) >= 1): img_name = args[0]
+    else: quit("É necessário carregar uma imagem para executar a aplicação!")
+    create_hist = False
+    is_diff_abs = True
+    is_verbose = False
+    l_inicial = 127
+    diff_limite = 5
+    for i in range(1, len(args)):
+        if (args[i] == "-l" and args[i+1] is not None):
+            l_inicial = int(args[i+1])
+        if (args[i] == "-dl" and args[i+1] is not None):
+            diff_limite = int(args[i+1])
+        if (args[i] == "-h"):
+            create_hist = True
+        if (args[i] == "-dr"):
+            is_diff_abs = False
+        if (args[i] == "-da"):
+            is_diff_abs = True
+        if (args[i] == "-v"):
+            is_verbose = True
+    if (is_verbose):
+        print("Limiar inicial: ", l_inicial)
+        print("Diferença limite: ", diff_limite)
+        print("Diferença absoluta? ", is_diff_abs)
+    return img_name, l_inicial, diff_limite, is_diff_abs, create_hist, is_verbose
 	
 
-def calc_limiar(img, n_linhas, n_colunas, l, err, is_diff_abs):
-	somaMenor = somaMaior = contaMenor = contaMaior = 0
+def calc_limiar(img, img_size, l, err, is_diff_abs):
 	diff = err
 	while (diff >= err):
-		for i in range(n_linhas):
-			for j in range(n_colunas):
-				if (img[i][j] < l):
-					somaMenor += img[i][j]
-					contaMenor += 1
-				else:
-					somaMaior += img[i][j]
-					contaMaior += 1
+		somaMenor = somaMaior = contaMenor = contaMaior = 0
+		for i in range(img_size):
+			if (img[i] < l):
+				somaMenor += img[i]
+				contaMenor += 1
+			else:
+				somaMaior += img[i]
+				contaMaior += 1
 		l_maior = somaMaior/contaMaior
 		l_menor = somaMenor/contaMenor
 		l1 = (l_maior+l_menor)/2
@@ -53,51 +54,54 @@ def calc_limiar(img, n_linhas, n_colunas, l, err, is_diff_abs):
 		l = l1
 	return l
 
-def calc_hist(img, n_linhas, n_colunas):
+def calc_hist(img, img_size):
 	hist = [0]*256
-	for i in range(n_linhas):
-		for j in range(n_colunas):
-			hist[img[i][j]] += 1
+	for i in range(img_size):
+		hist[img[i]] += 1
 	return hist
 
-def create_binary(img, n_linhas, n_colunas, l):
-	new_img = np.array([np.array([0]*n_colunas)]*n_linhas)
-	for i in range(n_linhas):
-		for j in range(n_colunas):
-			if (img[i][j] > l): 
-				new_img[i][j] = 255
+def create_binary(img, img_size, l):
+	new_img = np.array([0]*img_size)
+	for i in range(img_size):
+		if (img[i] > l): 
+			new_img[i] = 255
 	return new_img
 
 def main(argv):
 	img_path, l_inicial, diff_limite, is_diff_abs, create_hist, is_verbose = parse_args(argv)
 	img = cv.imread(img_path, 0)
 	n_linhas, n_colunas = img.shape
+	img_v = img.reshape(n_linhas*n_colunas)
 
 	if (is_verbose):
-		start = time.perf_counter()
-		l = calc_limiar(img, n_linhas, n_colunas, l_inicial, diff_limite, is_diff_abs)
+		print("-------------------------------------------------------")
+		print("Iniciando...")
+		start = start_total = time.perf_counter()
+		l = calc_limiar(img_v, img_v.size, l_inicial, diff_limite, is_diff_abs)
 		finish = time.perf_counter()
 		print("Limiar: ", l)
 		print("Cálculo do limiar feito em {} segundos".format(finish-start))
 	else:
-		l = calc_limiar(img, n_linhas, n_colunas, l_inicial, diff_limite, is_diff_abs)
+		l = calc_limiar(img_v, img_v.size, l_inicial, diff_limite, is_diff_abs)
 	
 	if (is_verbose):
 		start = time.perf_counter()
-		hist = calc_hist(img, n_linhas, n_colunas)
+		hist = calc_hist(img_v, img_v.size)
 		finish = time.perf_counter()
 		print("Cálculo do histograma feito em {} segundos".format(finish-start))
 	else:
-		hist = calc_hist(img, n_linhas, n_colunas)
+		hist = calc_hist(img_v, img_v.size)
 	
 	if (is_verbose):
 		start = time.perf_counter()
-		new_img = create_binary(img, n_linhas, n_colunas, l)
-		finish = time.perf_counter()
+		new_img = create_binary(img_v, img_v.size, l)
+		finish = finish_total = time.perf_counter()
 		print("Binarização da imagem feita em {} segundos".format(finish-start))
+		print("Tempo de execução total: {} segundos".format(finish_total-start_total))
 	else:
-		new_img = create_binary(img, n_linhas, n_colunas, l)
+		new_img = create_binary(img_v, img_v.size, l)
 	
+	new_img = new_img.reshape((n_linhas, n_colunas))
 	img_name, img_ext = img_path.split('.')[-2:]
 	img_name = img_name.split('/')[-1]
 	if (create_hist):
