9831764c398f1387a3d17fcfa64273fb7f8fd7de
Matheus Andrade <mathews754@gmail.com>
2020-11-19 11:14:06 -0300

    Alterações na documentação

--- Limiarizacao/limiar_seq_old.py
+++ Limiarizacao/limiar_seq_old.py
@@ -1,7 +1,7 @@
 import cv2 as cv
 import numpy as np
 from matplotlib import pyplot as plt
-import pymp
+
 import sys
 import time
 
@@ -13,12 +13,11 @@ def parse_args(args):
     is_verbose = False
     l_inicial = 127
     diff_limite = 5
-    n_threads = pymp.config.num_threads[0]
     for i in range(1, len(args)):
         if (args[i] == "-l" and args[i+1] is not None):
             l_inicial = int(args[i+1])
         if (args[i] == "-dl" and args[i+1] is not None):
-            diff_limite = float(args[i+1])
+            diff_limite = int(args[i+1])
         if (args[i] == "-h"):
             create_hist = True
         if (args[i] == "-dr"):
@@ -27,26 +26,27 @@ def parse_args(args):
             is_diff_abs = True
         if (args[i] == "-v"):
             is_verbose = True
-        if (args[i] == "-nt" and args[i+1] is not None):
-            n_threads = int(args[i+1])
     if (is_verbose):
-        print("Limiar inicial:", l_inicial)
-        print("Diferença limite:", diff_limite)
-        print("Diferença absoluta?", is_diff_abs)
-        print("Número de threads utilizados:", n_threads)
-    return img_name, l_inicial, diff_limite, is_diff_abs, create_hist, is_verbose, n_threads
+        print("Limiar inicial: ", l_inicial)
+        print("Diferença limite: ", diff_limite)
+        print("Diferença absoluta? ", is_diff_abs)
+    return img_name, l_inicial, diff_limite, is_diff_abs, create_hist, is_verbose
+	
 
-def calc_limiar(hist, l, err, is_diff_abs):
+def calc_limiar(img, img_size, l, err, is_diff_abs):
 	diff = err
 	while (diff >= err):
 		somaMenor = somaMaior = contaMenor = contaMaior = 0
-		for i in range(int(l)):
-			somaMenor += hist[i]*i
-			contaMenor += hist[i]
-		for i in range(int(l), 256):
-			somaMaior += hist[i]*i
-			contaMaior += hist[i]
-		l1 = (somaMaior/contaMaior+somaMenor/contaMenor)/2
+		for i in range(img_size):
+			if (img[i] < l):
+				somaMenor += img[i]
+				contaMenor += 1
+			else:
+				somaMaior += img[i]
+				contaMaior += 1
+		l_maior = somaMaior/contaMaior
+		l_menor = somaMenor/contaMenor
+		l1 = (l_maior+l_menor)/2
 		if (is_diff_abs): 
 			diff = abs(l1 - l)
 		else:
@@ -54,60 +54,52 @@ def calc_limiar(hist, l, err, is_diff_abs):
 		l = l1
 	return l
 
-def calc_hist(img, img_size, n_threads):
-    hist = pymp.shared.array((256,), dtype='uint32')
-    with pymp.Parallel(n_threads) as p:
-        p_hist = [0]*256
-        for i in p.range(img_size):
-            p_hist[img[i]] += 1
-        for i in range(256):
-            with p.lock:
-                hist[i] += p_hist[i]
-    return hist
+def calc_hist(img, img_size):
+	hist = [0]*256
+	for i in range(img_size):
+		hist[img[i]] += 1
+	return hist
 
-def create_binary(img, img_size, l, n_threads):
-    new_img = pymp.shared.array((img_size,), dtype='uint8')
-    with pymp.Parallel(n_threads) as p:
-        for i in p.range(img_size):
-            if (img[i] > l): 
-                new_img[i] = 255
-            else: 
-                new_img[i] = 0
-    return new_img
+def create_binary(img, img_size, l):
+	new_img = np.array([0]*img_size)
+	for i in range(img_size):
+		if (img[i] > l): 
+			new_img[i] = 255
+	return new_img
 
 def main(argv):
-	img_path, l_inicial, diff_limite, is_diff_abs, create_hist, is_verbose, n_threads = parse_args(argv)
+	img_path, l_inicial, diff_limite, is_diff_abs, create_hist, is_verbose = parse_args(argv)
 	img = cv.imread(img_path, 0)
 	n_linhas, n_colunas = img.shape
 	img_v = img.reshape(n_linhas*n_colunas)
-    
+
 	if (is_verbose):
 		print("-------------------------------------------------------")
 		print("Iniciando...")
 		start = start_total = time.perf_counter()
-		hist = calc_hist(img_v, img_v.size, n_threads)
+		l = calc_limiar(img_v, img_v.size, l_inicial, diff_limite, is_diff_abs)
 		finish = time.perf_counter()
-		print("Cálculo do histograma feito em {} segundos".format(finish-start))
+		print("Limiar: ", l)
+		print("Cálculo do limiar feito em {} segundos".format(finish-start))
 	else:
-		hist = calc_hist(img_v, img_v.size, n_threads)
+		l = calc_limiar(img_v, img_v.size, l_inicial, diff_limite, is_diff_abs)
 	
 	if (is_verbose):
 		start = time.perf_counter()
-		l = calc_limiar(hist, l_inicial, diff_limite, is_diff_abs)
+		hist = calc_hist(img_v, img_v.size)
 		finish = time.perf_counter()
-		print("Limiar: ", l)
-		print("Cálculo do limiar feito em {} segundos".format(finish - start))
+		print("Cálculo do histograma feito em {} segundos".format(finish-start))
 	else:
-		l = calc_limiar(hist, l_inicial, diff_limite, is_diff_abs)
-    
+		hist = calc_hist(img_v, img_v.size)
+	
 	if (is_verbose):
 		start = time.perf_counter()
-		new_img = create_binary(img_v, img_v.size, l, n_threads)
+		new_img = create_binary(img_v, img_v.size, l)
 		finish = finish_total = time.perf_counter()
 		print("Binarização da imagem feita em {} segundos".format(finish-start))
 		print("Tempo de execução total: {} segundos".format(finish_total-start_total))
 	else:
-		new_img = create_binary(img_v, img_v.size, l, n_threads)
+		new_img = create_binary(img_v, img_v.size, l)
 	
 	new_img = new_img.reshape((n_linhas, n_colunas))
 	img_name, img_ext = img_path.split('.')[-2:]
